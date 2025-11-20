bd088caa128782f7fff03d452a2a6e0622df6487
VincentChen1204 <vincentc273@nycstudents.net>
2025-11-20 13:16:47 -0500

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,20 @@
+public class Main {
+    public static void main(String[] args) {
+
+        /** Game 1, word: mississippi */
+        WordMatch w = new WordMatch("mississippi");
+        System.out.println(w.scoreGuess("i"));
+        System.out.println(w.scoreGuess("iss"));
+        System.out.println(w.scoreGuess("issipp"));
+        System.out.println(w.scoreGuess("mississippi"));
+
+        /** Game 2, word: concatenation */
+        WordMatch game = new WordMatch("concatenation");
+        System.out.println(game.scoreGuess("ten"));
+        System.out.println(game.scoreGuess("nation"));
+        System.out.println(game.findBetterGuess("ten", "nation"));
+        System.out.println(game.scoreGuess("con"));
+        System.out.println(game.scoreGuess("cat"));
+        System.out.println(game.findBetterGuess("con", "cat"));
+    }
+}
